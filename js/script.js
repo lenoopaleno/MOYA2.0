378d72e5fc1d6af3ad6df06078ac3fc03553bd9d
lenoopaleno <jjczapski@gmail.com>
2023-02-24 23:37:20 +0100

    Galeria

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
-
 addEventListener('DOMContentLoaded', () => {
 
-    const primaryNav = document.querySelector('.primary-navigation');
-    const navToggle = document.querySelector('.mobile-nav-toggle');
+        const primaryNav = document.querySelector('.primary-navigation');
+        const navToggle = document.querySelector('.mobile-nav-toggle');
 
-    navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+        navToggle.addEventListener("click", () => {
+            const visibility = primaryNav.getAttribute('data-visible');
 
-    if (visibility === "false"){
-        primaryNav.setAttribute('data-visible', true)
-        navToggle.setAttribute('aria-expanded', true)
-    } else if (visibility === "true") {
-        primaryNav.setAttribute('data-visible', false)
-        navToggle.setAttribute('aria-expanded', false)
-    }
-})
-})
-
-
+            if (visibility === "false") {
+                primaryNav.setAttribute('data-visible', true)
+                navToggle.setAttribute('aria-expanded', true)
+            } else if (visibility === "true") {
+                primaryNav.setAttribute('data-visible', false)
+                navToggle.setAttribute('aria-expanded', false)
+            }
+        })
+    })
